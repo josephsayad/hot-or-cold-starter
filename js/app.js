4bc7be5b71ff40b2e0cbb0afaa2938fe1f439852
Joseph Sayad <JosephSayad95@gmail.com>
2015-11-20 23:07:10 -0500

    Comments added and removed

--- js/app.js
+++ js/app.js
@@ -1,27 +1,43 @@
+// Switching the value of DEBUG will cease the firing of logs,
+// or accounts, of the code being executed: Used for debugging
 var DEBUG = true;
+
+// victoryCount is declared a global variable to be used as a means 
+// of restricting further gameplay: used in newGame(), restrictGamePlay()
+// victoryCount is incremented at each instance the user submits the 
+// proper guessed number and is re-assigned to zero after clicking a.new
 var victoryCount = 0;
 
-$(document).ready(function(){
+$(document).ready(function() {
   infoModalBox();
   newGame();
 });
 
 function infoModalBox() {
-  $('.what').click(function(){
+  $('.what').click(function() {
     $('.overlay').fadeIn(1000);
   });
-  $('a.close').click(function(){
+  $('a.close').click(function() {
     $('.overlay').fadeOut(1000);
   });
 }
 
 function newGame() {
+  // randomNumber is declared and, proceeding function invocation, is assigned 
+  // to the return value of randomNumberGenerator(): occurs only one time in 
+  // this instance of the program
   var randomNumber = randomNumberGenerator();
   var guessCount = 0, gameNumber = 1;
-  if (DEBUG) console.log('Random Number: '+ randomNumber + '\tGame: ' + gameNumber);
+
+  if (DEBUG) console.log('Random Number: '+ randomNumber + ' Game: ' + gameNumber);
+
   $('#guessButton').on('click', function() {
     var numericalGuess = $('input').val();
     var watchOut = hybridCase(numericalGuess);
+    // watchOut is a variable declared in the intention of tracking any string 
+    // values that contain both letters and numbers: it is assigned to the return
+    // value of the hybridCase() function, which assesses all userInput
+
     if (numericalGuess == '') {
       $('.prompt').show();
       $('.out-of-range').hide();
@@ -40,10 +56,6 @@ function newGame() {
       $('.feedback-output').hide();
       $('.out-of-range').show();
     }
-    // hybridCase will return false if not all characters of the userInput 
-    // evaluates to true when passed into parseInt(). This means a
-    // conditional that restricts normal program behaviors should be at play
-    // when watchOut is false; hence, else if !watchOut
     else if (!watchOut) {
       $('.prompt').hide();
       $('.feedback-output').hide();
@@ -63,17 +75,22 @@ function newGame() {
     victoryCount = 0;
     gameNumber++;
     randomNumber = randomNumberGenerator();
+    // games two through n recieve their random numbers at this instance of
+    // the program: at each instance the user opts to play a new game, a new 
+    // random number is returned and assigned to this variable  
 
     guessCount = 0;
     $('#guessList').find('li').remove();
     $('#count').replaceWith('<span id="count">'+ guessCount +'</span>');
-    if (DEBUG) console.log('Random Number: '+ randomNumber + '\tGame: ' + gameNumber);
+    if (DEBUG) console.log('Random Number: '+ randomNumber + ' Game: ' + gameNumber);
 
     $('.not-number').hide();
     $('.out-of-range').hide();
     $('.feedback-output').hide();
     $('.prompt').show();
-
+    
+    // Clears text field, input#userGuess, at each instance the user opts to 
+    // play a new game
     $('.text').val('');
   });
 
@@ -87,11 +104,9 @@ function randomNumberGenerator() {
 }
 
 function hotOrCold(numericalGuessParameter, randomNumberParameter) {
-
   var range = undefined;
 
   var feedback = function(rangeParameter) {
-
     if (rangeParameter == 0) {
       victoryCount++;
       return 'You Got It! Champ.'
@@ -123,8 +138,7 @@ function hotOrCold(numericalGuessParameter, randomNumberParameter) {
     range = randomNumberParameter - numericalGuessParameter;
   }
 
-  msg = feedback(range);
-
+  var msg = feedback(range);
   if (DEBUG) console.log('Guess: ' + numericalGuessParameter);
   $('.prompt').hide();
   $('.not-number').hide();
@@ -137,6 +151,12 @@ function addItem(numericalGuessParameter) {
   $('#guessList').append('<li>'+ numericalGuessParameter +'</li>');
 }
 
+// When the user enters a value, such as '2ej,' hybridCase() takes this value, as its
+// arguement, and assesses the string of three characters: hybridCase() takes each 
+// character, via for loop, and passes the individual characters into parseInt()
+// If parseInt() returns false, one of our three characters is a letter: hybridCase() 
+// returns 'false'; otherwise, it is 'true' our three letter string is a proper numerical 
+// guess 
 function hybridCase(userInput) {
   for (var i = 0, len = userInput.length; i < len; i++) {
     if (userInput[i] != 0) {
@@ -148,6 +168,7 @@ function hybridCase(userInput) {
   return true;
 }
 
+// Limits game play after the user submits the proper random number
 function restrictGamePlay(countRestriction) {
   if (victoryCount >= 2) {
     $('#guessList').find('li:last').hide();
